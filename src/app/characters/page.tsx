5f6d5ffbbb090007cd9832c69bb37df2d3b416d4
medbottle <cachorrogringo31@gmail.com>
2025-11-26 12:15:38 -0300

    feat: add back navigation link to main page

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,8 +1,13 @@
 import CharacterSearch from "../characters/search";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
 export default function CharactersPage() {
   return (
-    <main className="p-8 space-y-6">
+    <main className="p-8 space-y-6 relative min-h-screen">
+      <Link href="/">
+        <ArrowLeft className="fixed h-10 w-10 transition-transform duration-200 hover:scale-120 " />
+      </Link>
       <CharacterSearch />
     </main>
   );
