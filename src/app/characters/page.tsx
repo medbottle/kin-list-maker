a0ee46a7e8d220c6f155e0195eff6d64b0947561
medbottle <cachorrogringo31@gmail.com>
2025-11-26 11:41:41 -0300

    refactor: remove header from characters page

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -3,7 +3,6 @@ import CharacterSearch from "../characters/search";
 export default function CharactersPage() {
   return (
     <main className="p-8 space-y-6">
-      <h1 className="text-2xl font-bold">Add Characters</h1>
       <CharacterSearch />
     </main>
   );
