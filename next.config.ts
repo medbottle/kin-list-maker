e04e20483cef59c77145d5761914bad3e0ec0372
medbottle <cachorrogringo31@gmail.com>
2025-11-26 21:45:42 -0300

    chore: disable development indicators in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  devIndicators: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s4.anilist.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
