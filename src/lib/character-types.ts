2e88761d12693c30007e2549ed96dc65660995d9
medbottle <cachorrogringo31@gmail.com>
2025-11-27 07:52:29 -0300

    refactor: simplify character data structure by removing unused fields and standardizing media references

--- src/lib/character-types.ts
+++ src/lib/character-types.ts
@@ -1,23 +1,18 @@
-export type SourceType = "anime" | "tv" | "movie" | "game";
 export type SourceAPI = "anilist" | "tmdb" | "rawg";
 
 export interface Character {
   id: string;
   name: string;
   image: string | null;
-  source_name: string;
-  source_type: SourceType;
-  popularity: number | null;
+  media: string;
   source_api: SourceAPI;
 }
 
 export interface CharacterRow {
   id: string;
   name: string;
-  image_url: string | null;
-  source_name: string;
-  source_type: SourceType;
-  popularity: number | null;
+  image: string | null;
+  media: string;
   source_api: SourceAPI;
 }
 
