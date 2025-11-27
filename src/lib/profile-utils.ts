3778344d8bdff5787aada4cae40a723bdf9ffe83
medbottle <cachorrogringo31@gmail.com>
2025-11-27 03:47:30 -0300

    refactor: add location field to ProfileData for enhanced user profile information

--- src/lib/profile-utils.ts
+++ src/lib/profile-utils.ts
@@ -6,6 +6,7 @@ export type ProfileData = {
   gender: string | null;
   profilePicture: string | null;
   userNumber: string | null;
+  location: string | null;
 };
 
 export function extractProfileData(user: User): ProfileData {
@@ -15,6 +16,7 @@ export function extractProfileData(user: User): ProfileData {
     gender: user.user_metadata?.gender || null,
     profilePicture: user.user_metadata?.profile_picture || null,
     userNumber: userNumber,
+    location: user.user_metadata?.location || null,
   };
 }
 
